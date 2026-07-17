8d046c7b983cd97b8b74a229e2177929bf0eefde
antonyessac351-debug <antonyessac@gmail.com>
2026-07-17 22:41:56 +0300

    question_1

--- Program.cs
+++ Program.cs
@@ -1,10 +1,38 @@
-﻿namespace C__Session_1
+﻿using System.Reflection.Metadata;
+
+namespace C__Session_1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region
+            //Book book1 = new Book();
+
+            //    book1.title = "c# programming";
+            //    book1.pages = 512;
+
+            //object obj = book1;
+            //Console.WriteLine($"The Full Namespace is [ {obj} ]");
+
+            //Console.WriteLine("_____________________________________________");
+
+            //Console.WriteLine($"The Full Namespace is {book1.ToString}");  // ToString Method 
+
+            //Console.WriteLine("_____________________________________________");
+
+            //Console.WriteLine(book1.Equals(book1)); // True 
+
+            //Console.WriteLine("_____________________________________________");
+
+            //Console.WriteLine($"The Hashcode Is ---> {book1.GetHashCode()}");
+
+            //Console.WriteLine("_____________________________________________");
+
+            //Console.WriteLine($"The Type Is ----> {book1.GetType}");
+            #endregion
+
+
         }
     }
 }
