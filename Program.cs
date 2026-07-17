8bddcf3198685b3853a61e8507a6ee8b9ead0567
antonyessac351-debug <antonyessac@gmail.com>
2026-07-17 23:10:51 +0300

    question1

--- Program.cs
+++ Program.cs
@@ -7,29 +7,29 @@ namespace C__Session_1
         static void Main(string[] args)
         {
             #region
-            //Book book1 = new Book();
+            Book book1 = new Book();
 
-            //    book1.title = "c# programming";
-            //    book1.pages = 512;
+            book1.title = "c# programming";
+            book1.pages = 512;
 
-            //object obj = book1;
-            //Console.WriteLine($"The Full Namespace is [ {obj} ]");
+            object obj = book1;
+            Console.WriteLine($"The Full Namespace is [ {obj} ]");
 
-            //Console.WriteLine("_____________________________________________");
+            Console.WriteLine("_____________________________________________");
 
-            //Console.WriteLine($"The Full Namespace is {book1.ToString}");  // ToString Method 
+            Console.WriteLine($"The Full Namespace is {book1.ToString}");  // ToString Method 
 
-            //Console.WriteLine("_____________________________________________");
+            Console.WriteLine("_____________________________________________");
 
-            //Console.WriteLine(book1.Equals(book1)); // True 
+            Console.WriteLine(book1.Equals(book1)); // True 
 
-            //Console.WriteLine("_____________________________________________");
+            Console.WriteLine("_____________________________________________");
 
-            //Console.WriteLine($"The Hashcode Is ---> {book1.GetHashCode()}");
+            Console.WriteLine($"The Hashcode Is ---> {book1.GetHashCode()}");
 
-            //Console.WriteLine("_____________________________________________");
+            Console.WriteLine("_____________________________________________");
 
-            //Console.WriteLine($"The Type Is ----> {book1.GetType}");
+            Console.WriteLine($"The Type Is ----> {book1.GetType}");
             #endregion
 
 
