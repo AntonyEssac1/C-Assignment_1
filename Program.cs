0f2dd862757edd3fba67673b9e36dc3b63a646db
antonyessac351-debug <antonyessac@gmail.com>
2026-07-17 23:14:09 +0300

    question1

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection.Metadata;
+using System.Runtime.Intrinsics.X86;
 
 namespace C__Session_1
 {
@@ -7,29 +8,46 @@ namespace C__Session_1
         static void Main(string[] args)
         {
             #region
-            Book book1 = new Book();
 
-            book1.title = "c# programming";
-            book1.pages = 512;
+            //Create a Book class with a Title (string) and Pages (int). Create a Book object and store it in a variable of type object. Print it.
 
-            object obj = book1;
-            Console.WriteLine($"The Full Namespace is [ {obj} ]");
 
-            Console.WriteLine("_____________________________________________");
+            //Book book1 = new Book();
 
-            Console.WriteLine($"The Full Namespace is {book1.ToString}");  // ToString Method 
+            //book1.title = "c# programming";
+            //book1.pages = 512;
 
-            Console.WriteLine("_____________________________________________");
+            //object obj = book1;
+            //Console.WriteLine($"The Full Namespace is [ {obj} ]");
 
-            Console.WriteLine(book1.Equals(book1)); // True 
+            //Console.WriteLine("_____________________________________________");
+            ////Using the Book class above, print the result of calling ToString(), Equals() (compare book with itself), GetHashCode(), and GetType() on book.
 
-            Console.WriteLine("_____________________________________________");
 
-            Console.WriteLine($"The Hashcode Is ---> {book1.GetHashCode()}");
+            //Console.WriteLine($"The Full Namespace is {book1.ToString}");  // ToString Method 
 
-            Console.WriteLine("_____________________________________________");
+            //Console.WriteLine("_____________________________________________");
 
-            Console.WriteLine($"The Type Is ----> {book1.GetType}");
+            //Console.WriteLine(book1.Equals(book1)); // True 
+
+            //Console.WriteLine("_____________________________________________");
+
+            //Console.WriteLine($"The Hashcode Is ---> {book1.GetHashCode()}");
+
+            //Console.WriteLine("_____________________________________________");
+
+            //Console.WriteLine($"The Type Is ----> {book1.GetType}");
+            #endregion
+
+            #region
+            //Look at the line below.Is it a compile - time error, a runtime error, or a logical error? Fix it.
+            // int pages = "464"; ------> exception Error(compile - time error)
+            // Fixes:
+
+            //int pages = 464;
+            //Console.WriteLine(pages);
+
+            //Console.WriteLine("---------------------------------------");
             #endregion
 
 
